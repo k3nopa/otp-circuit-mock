d2cb8f2423ad40e48230d93762d15203c3c665bd
k3nopa <afiqnaufalpro@gmail.com>
2021-12-17 19:29:29 +0900

    Add serial writer

--- otp-conn/src/main.rs
+++ otp-conn/src/main.rs
@@ -1,5 +1,10 @@
+extern crate serial;
+
 use otp::tree::DecisionTree;
 use rand::Rng;
+use serial::prelude::*;
+use std::io;
+use std::time::Duration;
 
 fn main() {
     let mut otp_tree = DecisionTree::new(128, 7);
@@ -12,11 +17,27 @@ fn main() {
         println!("Iteration: {} Path: {} -> {:?}", i, bin_repr, otp_key);
     }
 
-    // TODO: Need to implement global allocator in order to use Vec type for STM32 (or generate
-    // decision tree as static data(bss) in memory layout.
-    // REF : https://github.com/rust-embedded/alloc-cortex-m
-    // REF : https://github.com/yvt/rlsf
-    //
-    // TODO: Port to STM32 or Raspberry Pi.
-    // REF : https://github.com/golemparts/rppal
+    let mut port = serial::open("/dev/ttyS0").unwrap();
+    interact(&mut port).unwrap();
+}
+
+fn interact<T: SerialPort>(port: &mut T) -> io::Result<()> {
+    port.reconfigure(&|settings| {
+        settings.set_baud_rate(serial::Baud115200).unwrap();
+        settings.set_char_size(serial::Bits8);
+        settings.set_parity(serial::ParityNone);
+        settings.set_stop_bits(serial::Stop1);
+        settings.set_flow_control(serial::FlowNone);
+        Ok(())
+    })
+    .unwrap();
+
+    port.set_timeout(Duration::from_millis(1000)).unwrap();
+
+    let mut buf: Vec<u8> = (0..255).collect();
+
+    port.write(&buf[..]).unwrap();
+    //port.read(&mut buf[..]).unwrap();
+
+    Ok(())
 }
