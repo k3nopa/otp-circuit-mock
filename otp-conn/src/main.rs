e7f8b44885bc7463eca73bff0dffa4e971e8c69d
k3nopa <afiqnaufalpro@gmail.com>
2021-12-17 23:47:08 +0900

    Add serial reader & writer handler

--- otp-conn/src/main.rs
+++ otp-conn/src/main.rs
@@ -7,21 +7,7 @@ use std::io;
 use std::time::Duration;
 
 fn main() {
-    let mut otp_tree = DecisionTree::new(128, 7);
-
-    for i in 0..=10 {
-        let path = rand::thread_rng().gen_range(0..=255);
-        let bin_repr = format!("{:08b}", path);
-
-        let otp_key = otp_tree.key(path);
-        println!("Iteration: {} Path: {} -> {:?}", i, bin_repr, otp_key);
-    }
-
     let mut port = serial::open("/dev/ttyS0").unwrap();
-    interact(&mut port).unwrap();
-}
-
-fn interact<T: SerialPort>(port: &mut T) -> io::Result<()> {
     port.reconfigure(&|settings| {
         settings.set_baud_rate(serial::Baud115200).unwrap();
         settings.set_char_size(serial::Bits8);
@@ -31,13 +17,31 @@ fn interact<T: SerialPort>(port: &mut T) -> io::Result<()> {
         Ok(())
     })
     .unwrap();
+    let mut otp_tree = DecisionTree::new(128, 7);
 
+    loop {
+        let input = serial_recv(&mut port).unwrap();
+        let input = String::from_utf8_lossy(&input).to_string();
+        let path = input.parse::<u8>().unwrap();
+        println!("Path Recv: {}", path);
+
+        let otp_key = otp_tree.key(path).unwrap();
+        let key = otp_key.to_string();
+        serial_send(&mut port, key.as_bytes()).unwrap();
+    }
+}
+
+fn serial_send<T: SerialPort>(port: &mut T, key: &[u8]) -> io::Result<()> {
     port.set_timeout(Duration::from_millis(1000)).unwrap();
-
-    let mut buf: Vec<u8> = (0..255).collect();
-
-    port.write(&buf[..]).unwrap();
-    //port.read(&mut buf[..]).unwrap();
+    port.write(key).unwrap();
 
     Ok(())
+}
+
+fn serial_recv<T: SerialPort>(port: &mut T) -> io::Result<[u8; 50]> {
+    port.set_timeout(Duration::from_millis(1000)).unwrap();
+    let mut key = [0u8; 50];
+    port.read(&mut key[..]).unwrap();
+
+    Ok(key)
 }
