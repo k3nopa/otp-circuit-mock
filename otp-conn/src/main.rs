43940a2a26e8b490d646519d0fc57ae64aacaf91
k3nopa <afiqnaufalpro@gmail.com>
2021-12-18 15:03:41 +0900

    Port serial reader handler using serialport crate

--- otp-conn/src/main.rs
+++ otp-conn/src/main.rs
@@ -1,59 +1,63 @@
-extern crate serial;
-
 use otp::tree::DecisionTree;
-use rand::Rng;
-use serial::prelude::*;
+use serialport;
 use std::io;
 use std::time::Duration;
 
+enum Status {
+    ReadSize,
+    ReadData,
+}
+
 fn main() {
-    let mut port = serial::open("/dev/ttyS0").unwrap();
-    port.reconfigure(&|settings| {
-        settings.set_baud_rate(serial::Baud115200).unwrap();
-        settings.set_char_size(serial::Bits8);
-        settings.set_parity(serial::ParityNone);
-        settings.set_stop_bits(serial::Stop1);
-        settings.set_flow_control(serial::FlowNone);
-        Ok(())
-    })
-    .unwrap();
     let mut otp_tree = DecisionTree::new(128, 7);
+    let port_name = "/dev/ttyS0";
+    let baud_rate = 115200;
 
-    loop {
-        let input = serial_recv(&mut port).unwrap();
-        let path = input.parse::<u8>().unwrap();
-        println!("Path Recv: {}", path);
+    let port = serialport::new(port_name, baud_rate)
+        .timeout(Duration::from_millis(10))
+        .open();
 
-        let otp_key = otp_tree.key(path).unwrap();
-        let key = otp_key.to_string();
-        serial_send(&mut port, key.as_bytes()).unwrap();
-    }
-}
-
-fn serial_send<T: SerialPort>(port: &mut T, key: &[u8]) -> io::Result<()> {
-    port.set_timeout(Duration::from_millis(1000)).unwrap();
-    port.write(key).unwrap();
-
-    Ok(())
-}
-
-fn serial_recv<T: SerialPort>(port: &mut T) -> io::Result<String> {
-    port.set_timeout(Duration::from_millis(1000)).unwrap();
-    let mut key = [0u8; 5];
-    loop {
-        match port.read(&mut key[..]) {
-            Ok(_) => break,
-            Err(_) => port.set_timeout(Duration::from_millis(1000)).unwrap(),
-        };
-    }
-    println!("Byte Recv: {:?}", key);
-    let mut zero_index = 0;
-    for i in 0..key.len() {
-        if key[i] == 0u8 {
-            zero_index = i + 1;
-            break;
+    match port {
+        Ok(mut port) => {
+            println!("Receiving data on {} at {} baud:", &port_name, &baud_rate);
+            let path = serial_read(&mut port).unwrap();
+            println!("Path: {:?}", path);
+        }
+        Err(e) => {
+            eprintln!("Failed to open \"{}\". Error: {}", port_name, e);
+            ::std::process::exit(1);
         }
     }
-    let key = String::from_utf8(key[0..zero_index].to_vec()).unwrap();
-    Ok(key)
+}
+
+fn serial_read(port: &mut Box<dyn serialport::SerialPort>) -> io::Result<String> {
+    let mut status = Status::ReadSize;
+    let mut tmp_path = String::new();
+    let mut size = 0;
+    let mut buffer = [0; 10];
+
+    let path = loop {
+        match port.read(&mut buffer) {
+            Ok(t) => match status {
+                Status::ReadSize => {
+                    size = String::from_utf8_lossy(&buffer[..t])
+                        .parse::<usize>()
+                        .unwrap();
+                    status = Status::ReadData;
+                    tmp_path.reserve(size);
+                }
+                Status::ReadData => {
+                    size -= t;
+                    let input = String::from_utf8_lossy(&buffer[..t]);
+                    tmp_path += &input;
+                    if size == 0 {
+                        break tmp_path;
+                    }
+                }
+            },
+            Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
+            Err(e) => eprintln!("{:?}", e),
+        }
+    };
+    Ok(path)
 }
