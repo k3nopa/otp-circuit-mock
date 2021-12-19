2115ddcd1fbfe34357a8c86f0c9e3f88a7fb1c83
k3nopa <afiqnaufalpro@gmail.com>
2021-12-19 19:37:13 +0900

    Can send all keys from OTP trees through UART

--- otp-conn/src/main.rs
+++ otp-conn/src/main.rs
@@ -1,8 +1,8 @@
 use byteorder::{BigEndian, WriteBytesExt};
 use otp::tree::DecisionTree;
 use serialport;
-use std::io;
 use std::time::Duration;
+use std::{io, thread};
 
 enum Status {
     ReadSize,
@@ -16,37 +16,41 @@ fn main() {
     let port_name = "/dev/ttyS0";
     let baud_rate = 115200;
 
-    // let builder = serialport::new(port_name, baud_rate)
-    //     .stop_bits(serialport::StopBits::One)
-    //     .data_bits(serialport::DataBits::Eight)
-    //     .timeout(Duration::from_millis(10));
+    let builder = serialport::new(port_name, baud_rate)
+        .stop_bits(serialport::StopBits::One)
+        .data_bits(serialport::DataBits::Eight)
+        .timeout(Duration::from_millis(10));
 
-    // let port = builder.open();
+    let port = builder.open();
     let (mut otp_tree, mut memory_nodes) = DecisionTree::new(LAYER, HEIGHT);
 
-    let slice = &memory_nodes[0];
-    for val in slice {
-        let mut data = vec![];
-        data.write_u32::<BigEndian>(*val).unwrap();
-        println!("{}: {:?}", val, data);
-        //serial_write(&mut port, &data).unwrap();
+    match port {
+        Ok(mut port) => {
+            println!("UART on {} at {} baud:", &port_name, &baud_rate);
+            // 1. Send OTP info.
+            for node in memory_nodes {
+                for val in node {
+                    let mut data = vec![];
+                    data.write_u32::<BigEndian>(val).unwrap();
+
+                    println!("{}: {:?}", val, data);
+                    serial_write(&mut port, &data).unwrap();
+                    thread::sleep(Duration::from_millis(20));
+                }
+                thread::sleep(Duration::from_millis(5));
+            }
+            // serial_write(&mut port, memory_nodes).unwrap();
+            // let path = serial_read(&mut port).unwrap();
+            // println!("Path: {:?}", path);
+            // let path = path.parse::<u8>().unwrap();
+            // let res = otp_tree.key(path).unwrap();
+            // println!("Key: {:?}", res);
+        }
+        Err(e) => {
+            eprintln!("Failed to open \"{}\". Error: {}", port_name, e);
+            ::std::process::exit(1);
+        }
     }
-    // match port {
-    //     Ok(mut port) => {
-    //         println!("UART on {} at {} baud:", &port_name, &baud_rate);
-    //         // 1. Send OTP info.
-    //         // serial_write(&mut port, memory_nodes).unwrap();
-    //         // let path = serial_read(&mut port).unwrap();
-    //         // println!("Path: {:?}", path);
-    //         // let path = path.parse::<u8>().unwrap();
-    //         // let res = otp_tree.key(path).unwrap();
-    //         // println!("Key: {:?}", res);
-    //     }
-    //     Err(e) => {
-    //         eprintln!("Failed to open \"{}\". Error: {}", port_name, e);
-    //         ::std::process::exit(1);
-    //     }
-    // }
 }
 
 fn serial_read(port: &mut Box<dyn serialport::SerialPort>) -> io::Result<String> {
